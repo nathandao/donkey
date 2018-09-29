c4879f982356690d25876b2e84e3748d0811da36
Markus Paasovaara <markus.paasovaara@futurice.com>
2018-09-29 13:20:31 +0300

    Futucar model, based on linear

--- donkeycar/parts/keras.py
+++ donkeycar/parts/keras.py
@@ -9,7 +9,7 @@ functions to run and train autopilots using keras
 from tensorflow.python.keras.layers import Input
 from tensorflow.python.keras.models import Model, load_model
 from tensorflow.python.keras.layers import Convolution2D
-from tensorflow.python.keras.layers import Dropout, Flatten, Dense, Cropping2D, Lambda
+from tensorflow.python.keras.layers import Dropout, Flatten, Dense, Cropping2D, Lambda, MaxPool2D
 from tensorflow.python.keras.callbacks import ModelCheckpoint, EarlyStopping
 
 from donkeycar import util
@@ -58,6 +58,7 @@ class KerasPilot:
             validation_data=val_gen,
             callbacks=callbacks_list,
             validation_steps=steps * (1.0 - train_split) / train_split)
+
         return hist
 
 
@@ -86,8 +87,10 @@ class KerasLinear(KerasPilot):
         #    self.model = default_n_linear(num_outputs)
         #else:
         #    self.model = default_linear()
-        self.model = default_linear()
 
+        #self.model = default_linear()
+        self.model = futucar_model()
+        self.model.summary()
     def run(self, img_arr):
         img_arr = img_arr.reshape((1,) + img_arr.shape)
         outputs = self.model.predict(img_arr)
@@ -252,6 +255,71 @@ def default_linear():
 
     return model
 
+def futucar_model():
+    img_in = Input(shape=(120, 160, 3), name='img_in')
+    x = img_in
+
+    # Convolution2D class name is an alias for Conv2D
+    x = Convolution2D(filters=24, kernel_size=(5, 5), strides=(2, 2), activation='relu')(x)
+    x = Convolution2D(filters=32, kernel_size=(5, 5), strides=(2, 2), activation='relu')(x)
+    x = Convolution2D(filters=64, kernel_size=(5, 5), strides=(2, 2), activation='relu')(x)
+    x = Dropout(rate=.1)(x)
+    x = Convolution2D(filters=64, kernel_size=(3, 3), strides=(2, 2), activation='relu')(x)
+    x = Convolution2D(filters=64, kernel_size=(3, 3), strides=(1, 1), activation='relu')(x)
+
+    x = Flatten(name='flattened')(x)
+    x = Dense(units=100, activation='linear')(x)
+    x = Dropout(rate=.1)(x)
+    x = Dense(units=50, activation='linear')(x)
+    x = Dropout(rate=.1)(x)
+    # categorical output of the angle
+    angle_out = Dense(units=1, activation='linear', name='angle_out')(x)
+
+    # continous output of throttle
+    throttle_out = Dense(units=1, activation='linear', name='throttle_out')(x)
+
+    model = Model(inputs=[img_in], outputs=[angle_out, throttle_out])
+
+    model.compile(optimizer='adam',
+                  loss={'angle_out': 'mean_squared_error',
+                        'throttle_out': 'mean_squared_error'},
+                  loss_weights={'angle_out': 0.8, 'throttle_out': 0.2})
+
+    return model
+
+'''
+def futucar_model2():
+    img_in = Input(shape=(120, 160, 3), name='img_in')
+    x = img_in
+
+    # Convolution2D class name is an alias for Conv2D
+    x = Convolution2D(filters=24, kernel_size=(5, 5), strides=(2, 2), activation='relu')(x)
+    x = Convolution2D(filters=32, kernel_size=(5, 5), strides=(2, 2), activation='relu')(x)
+    x = Convolution2D(filters=64, kernel_size=(5, 5), strides=(2, 2), activation='relu')(x)
+    x = MaxPool2D(pool_size=(2, 2), data_format="channels_first")(x)
+    x = Convolution2D(filters=64, kernel_size=(3, 3), strides=(2, 2), activation='relu')(x)
+    x = Convolution2D(filters=64, kernel_size=(3, 3), strides=(1, 1), activation='relu')(x)
+
+    x = Flatten(name='flattened')(x)
+    x = Dense(units=100, activation='linear')(x)
+    x = Dropout(rate=.1)(x)
+    x = Dense(units=50, activation='linear')(x)
+    x = Dropout(rate=.1)(x)
+    # categorical output of the angle
+    angle_out = Dense(units=1, activation='linear', name='angle_out')(x)
+
+    # continous output of throttle
+    throttle_out = Dense(units=1, activation='linear', name='throttle_out')(x)
+
+    model = Model(inputs=[img_in], outputs=[angle_out, throttle_out])
+
+    model.compile(optimizer='adam',
+                  loss={'angle_out': 'mean_squared_error',
+                        'throttle_out': 'mean_squared_error'},
+                  loss_weights={'angle_out': 0.8, 'throttle_out': 0.2})
+
+    return model
+'''
 
 def default_n_linear(num_outputs):
     img_in = Input(shape=(120, 160, 3), name='img_in')
