994b5ca2f13ad8b18f92584da23a87a15983a306
Markus Paasovaara <markus.paasovaara@futurice.com>
2018-10-06 17:24:26 +0300

    Mask in only interesting parts in color image

--- donkeycar/parts/preprocessing.py
+++ donkeycar/parts/preprocessing.py
@@ -30,7 +30,7 @@ def plot_canny(img):
 
 
     small_kernel = np.ones((3, 3), np.uint8)
-    large_kernel = np.ones((11, 11), np.uint8)
+    large_kernel = np.ones((9, 9), np.uint8)
     #erosion = cv2.erode(edges, kernel, iterations=1)
 
     closed_small = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, small_kernel)
@@ -40,8 +40,8 @@ def plot_canny(img):
     closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, large_kernel)
     plt.subplot(2, 2, 4), plt.imshow(closed, cmap='gray')
     plt.title(' '), plt.xticks([]), plt.yticks([])
-
-
+    plt.show()
+    return closed
     #opened = cv2.morphologyEx(edges, cv2.MORPH_OPEN, small_kernel)
     #opened_and_closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, large_kernel)
     #plt.subplot(2, 2, 4), plt.imshow(opened_and_closed, cmap='gray')
@@ -50,18 +50,36 @@ def plot_canny(img):
     #plt.subplot(2, 2, 4), plt.imshow(opened, cmap='gray')
     #plt.title(' '), plt.xticks([]), plt.yticks([])
 
+
+
+def blend_using_mask(img, img_mask):
+    ret, mask = cv2.threshold(img_mask, 10, 255, cv2.THRESH_BINARY)
+    output = cv2.bitwise_and(img, img, mask=mask)
+    plt.subplot(2, 2, 1), plt.imshow(img, cmap='gray')
+    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2, 2, 2), plt.imshow(img_mask, cmap='gray')
+    plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+    plt.subplot(2, 2, 3), plt.imshow(output, cmap='gray')
+    plt.title('Blended'), plt.xticks([]), plt.yticks([])
     plt.show()
 
 print(cv2.__version__)
 
 # Load an color image in grayscale
 img = cv2.imread('/Users/mpaa/donkey-data/data/simulator/test.jpg', 0)
-#img = cv2.imread('/Users/mpaa/donkey-data/data/simulator/test.jpg', cv2.IMREAD_COLOR)
+
+color_img = cv2.imread('/Users/mpaa/donkey-data/data/simulator/test.jpg', cv2.IMREAD_COLOR)
+#Matplotlib uses brg so color transformation is needed
+RGB_img = cv2.cvtColor(color_img, cv2.COLOR_BGR2RGB)
+
 #cv2.imshow('donkey',img)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
 
-plot_canny(img)
+mask = plot_canny(img)
+blend_using_mask(RGB_img, mask)
+
 #plot_sobel(img)
 
 #img = cv2.imread('dave.jpg',0)
